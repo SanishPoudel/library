6197ad03dceef67e0963f0257d495b281577cca2
SanishPoudel <mr.sanish.poudel@gmail.com>
2024-10-29 12:08:38 +1100

    need to fix the repeating elements

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const container = document.querySelector(".container");
 const new_btn = document.querySelector(".new_btn");
 const dialog = document.querySelector("dialog");
 const close = document.querySelector(".close");
+const submit = document.querySelector(".submit");
+const book_title = document.querySelector("#name_of_book");
+const author_name = document.querySelector("#author");
+const form = document.querySelector("form");
 
 // an array to hold the book objects
 const myLibrary = [];
@@ -16,6 +20,53 @@ function Book(name, author) {
 function addBookToLibrary(book) {
   // function to take user input and add them to the myLibrary array.
   myLibrary.push(book);
+  myLibrary.forEach((book, index) => {
+    //child elements
+    let writer = document.createElement("div");
+    let book_name = document.createElement("div");
+    let remove_button = document.createElement("button");
+    let read_button = document.createElement("button");
+    
+    // child elements' text
+    writer.textContent = "Author: " + book.author;
+    book_name.textContent = "Title: " + book.name;
+    remove_button.textContent = "Remove";
+    read_button.textContent = "Read";
+  
+    // making newbook which will act as a parent
+    let newbook = document.createElement("div");
+    newbook.setAttribute("data-index", index);
+  
+    //appending child elements into newbook
+    newbook.appendChild(book_name);
+    newbook.appendChild(writer);
+    newbook.appendChild(read_button);
+    newbook.appendChild(remove_button);
+  
+    // adding the remove button event listener
+    remove_button.addEventListener("click", () => {
+      myLibrary.splice(newbook.data-index, 1);
+      newbook.remove();
+    })
+  
+    // toggling the reading status
+    read_button.addEventListener("click", () => {
+      if (read_button.textContent === "Read") {
+        read_button.textContent = "Not read";
+      } else {
+        read_button.textContent = "Read";
+      }
+    })
+  
+    newbook.style.border = "1px solid black";
+    newbook.style.padding = "10px";
+    newbook.style.display = "flex";
+    newbook.style.flexDirection = "column";
+    newbook.style.gap = "5px";
+    
+    // adding newbook to the container to be displayed
+    container.appendChild(newbook); 
+  });
 }
 
 new_btn.addEventListener("click", () => {
@@ -27,45 +78,18 @@ close.addEventListener("click", () => {
   dialog.close();
 })
 
-book_1 = new Book("a", "b");
-book_2 = new Book("c", "d");
-addBookToLibrary(book_1);
-addBookToLibrary(book_2);
 
-console.log(myLibrary);
 
-myLibrary.forEach((book, index) => {
-  //child elements
-  let writer = document.createElement("div");
-  let book_name = document.createElement("div");
-  let remove_button = document.createElement("button");
-  
-  // child elements' text
-  writer.textContent = "Author: " + book.author;
-  book_name.textContent = "Title: " + book.name;
-  remove_button.textContent = "Remove";
+submit.addEventListener("click", input)
 
-  // making newbook which will act as a parent
-  let newbook = document.createElement("div");
-  newbook.setAttribute("data-index", index);
-
-  //appending child elements into newbook
-  newbook.appendChild(book_name);
-  newbook.appendChild(writer);
-  newbook.appendChild(remove_button);
-
-  // adding the remove button event listener
-  remove_button.addEventListener("click", () => {
-    myLibrary.splice(newbook.data-index, 1);
-    newbook.remove();
-  })
-
-  newbook.style.border = "1px solid black";
-  newbook.style.padding = "10px";
-  newbook.style.display = "flex";
-  newbook.style.flexDirection = "column";
-  newbook.style.gap = "5px";
-  
-  // adding newbook to the container to be displayed
-  container.appendChild(newbook); 
-});
+function input(event) {
+  event.preventDefault();
+  if (book_title.value != "" && author_name.value != "") {
+    let item = new Book(book_title.value, author_name.value);
+    addBookToLibrary(item);
+    form.reset()
+    dialog.close();
+  } else {
+    alert("Please fill out the forms correctly"); 
+  }
+}
